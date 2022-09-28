4f39a7c3c34bbe8258a45b28afd83a87eab36182
AnubhavSinghChandel <anubhavsinghchandel1498@gmail.com>
2022-09-28 11:25:06 +0530

    first commit

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,11 +1,8 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
   @override
-  State<HomePage> createState() => _HomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
@@ -17,45 +14,44 @@ class _HomePageState extends State<HomePage> {
   late String message;
   late List<String> gameState;
 
-  //initState
+  @override
   void initState() {
     super.initState();
-    message = " ";
     setState(() {
       this.gameState = [
-        "Empty",
-        "Empty",
-        "Empty",
-        "Empty",
-        "Empty",
-        "Empty",
-        "Empty",
-        "Empty",
-        "Empty"
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty"
       ];
+      this.message = "";
     });
   }
 
-  //resetGame
   resetGame() {
     setState(() {
       this.gameState = [
-        "Empty",
-        "Empty",
-        "Empty",
-        "Empty",
-        "Empty",
-        "Empty",
-        "Empty",
-        "Empty",
-        "Empty"
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty"
       ];
+      this.message = '';
     });
   }
 
-  //PlayGame
   playGame(int index) {
-    if (this.gameState[index] == "Empty") {
+    if (this.gameState[index] == "empty") {
       setState(() {
         if (this.isCross) {
           this.gameState[index] = 'cross';
@@ -68,70 +64,70 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-  //getImage
   AssetImage getImage(String value) {
     switch (value) {
-      case ('Empty'):
+      case ('empty'):
         return edit;
+
       case ('cross'):
         return cross;
+
       case ('circle'):
         return circle;
     }
     return edit;
   }
 
-  //checkWin
   checkWin() {
-    if ((gameState[0] != 'Empty') &&
+    if ((gameState[0] != 'empty') &&
         (gameState[0] == gameState[1]) &&
         (gameState[1] == gameState[2])) {
       setState(() {
-        this.message = 'Hurray!! ${this.gameState[0]} wins';
+        this.message = 'Hurray!! ${this.gameState[0]} Wins';
       });
-    } else if ((gameState[3] != 'Empty') &&
+    } else if ((gameState[3] != 'empty') &&
         (gameState[3] == gameState[4]) &&
         (gameState[4] == gameState[5])) {
       setState(() {
-        this.message = 'Hurray!! ${this.gameState[3]} wins';
+        this.message = 'Hurray!! ${this.gameState[3]} Wins';
       });
-    } else if ((gameState[6] != 'Empty') &&
+    } else if ((gameState[6] != 'empty') &&
         (gameState[6] == gameState[7]) &&
         (gameState[7] == gameState[8])) {
       setState(() {
-        this.message = 'Hurray!! ${this.gameState[6]} wins';
+        this.message = 'Hurray!! ${this.gameState[6]} Wins';
       });
-    } else if ((gameState[0] != 'Empty') &&
+    } else if ((gameState[0] != 'empty') &&
         (gameState[0] == gameState[3]) &&
         (gameState[3] == gameState[6])) {
       setState(() {
-        this.message = 'Hurray!! ${this.gameState[0]} wins';
+        this.message = 'Hurray!! ${this.gameState[0]} Wins';
       });
-    } else if ((gameState[1] != 'Empty') &&
+    } else if ((gameState[1] != 'empty') &&
         (gameState[1] == gameState[4]) &&
         (gameState[4] == gameState[7])) {
       setState(() {
-        this.message = 'Hurray!! ${this.gameState[1]} wins';
+        this.message = 'Hurray!! ${this.gameState[1]} Wins';
       });
-    } else if ((gameState[2] != 'Empty') &&
+    } else if ((gameState[2] != 'empty') &&
         (gameState[2] == gameState[5]) &&
         (gameState[5] == gameState[8])) {
       setState(() {
-        this.message = 'Hurray!! ${this.gameState[2]} wins';
+        this.message = 'Hurray!! ${this.gameState[2]} Wins';
       });
-    } else if ((gameState[0] != 'Empty') &&
+    } else if ((gameState[0] != 'empty') &&
         (gameState[0] == gameState[4]) &&
         (gameState[4] == gameState[8])) {
       setState(() {
-        this.message = 'Hurray!! ${this.gameState[0]} wins';
+        this.message = 'Hurray!! ${this.gameState[0]} Wins';
       });
-    } else if ((gameState[2] != 'Empty') &&
+    } else if ((gameState[2] != 'empty') &&
         (gameState[2] == gameState[4]) &&
         (gameState[4] == gameState[6])) {
       setState(() {
-        this.message = 'Hurray!! ${this.gameState[2]} wins';
+        this.message = 'Hurray!! ${this.gameState[2]} Wins';
       });
-    } else if (gameState.contains('Empty')) {
+    } else if (!gameState.contains('empty')) {
       setState(() {
         this.message = 'Game Draw';
       });
@@ -150,26 +146,26 @@ class _HomePageState extends State<HomePage> {
         children: <Widget>[
           Expanded(
             child: GridView.builder(
-              padding: EdgeInsets.all(20.0),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                crossAxisSpacing: 10.0,
-                childAspectRatio: 1.0,
-                mainAxisSpacing: 10.0,
-              ),
-              itemCount: this.gameState.length,
-              itemBuilder: (context, i) => SizedBox(
-                  width: 100.0,
-                  height: 100.0,
-                  child: MaterialButton(
-                    onPressed: () {
-                      this.playGame(i);
-                    },
-                    child: Image(
-                      image: this.getImage(this.gameState[i]),
-                    ),
-                  )),
-            ),
+                padding: EdgeInsets.all(20.0),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 1.0,
+                  crossAxisSpacing: 10.0,
+                  mainAxisSpacing: 10.0,
+                ),
+                itemCount: this.gameState.length,
+                itemBuilder: (context, i) => SizedBox(
+                      width: 100.0,
+                      height: 100.0,
+                      child: MaterialButton(
+                        onPressed: () {
+                          this.playGame(i);
+                        },
+                        child: Image(
+                          image: this.getImage(this.gameState[i]),
+                        ),
+                      ),
+                    )),
           ),
           Container(
             padding: EdgeInsets.all(20.0),
@@ -199,7 +195,7 @@ class _HomePageState extends State<HomePage> {
           Container(
             padding: EdgeInsets.all(20.0),
             child: Text(
-              "Made By Anubhav Singh Chandel",
+              "Made by Anubhav Singh Chandel",
               style: TextStyle(fontSize: 18.0),
             ),
           )
--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,11 @@ import 'HomePage.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Tic Tac Toe Game",
+      title: "Tic Tac Toe",
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.purple),
       home: HomePage(),
     );
